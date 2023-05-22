4b284f3942cb8af01130c6dac27d9e0c402ad692
Keeanu Rodriguez <keeanu7@gmail.com>
2023-05-22 03:24:43 -0400

    check

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 function Logo({size}) {
   return (
+    <div>
     <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
@@ -15,6 +16,24 @@ function Logo({size}) {
       clipRule="evenodd"
     />
   </svg>
+  <svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 20 20"
+  fill="#000000"
+  className="h-6 w-6 mx-auto my-auto"
+  stroke="#000000"
+>
+  <path
+    fillRule="evenodd"
+    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+    clipRule="evenodd"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="0"
+  />
+</svg>
+</div>
+
   )
 }
 
