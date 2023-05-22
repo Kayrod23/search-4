57eba531c22b855f464ea0da7a290af74e6394d9
Keeanu Rodriguez <keeanu7@gmail.com>
2023-05-22 03:34:19 -0400

    another check

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({size}) {
   return (
-    <div>
+    <div className='w-12 h-12'>
     <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
